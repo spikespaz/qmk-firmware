f80ce94d56ac678a989a85e314423c324914e3c7
Jacob Birkett <jacob_birkett@fourier.earth>
2024-12-28 22:51:16 -0600

    kb: nuphy: air75_v2: km: spikespaz: reorder left gui, ctrl, alt

--- keyboards/nuphy/air75_v2/ansi/keymaps/spikespaz/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/spikespaz/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,       KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_LBRC,    KC_RBRC,                KC_BSLS,    KC_END,
     KC_CAPS,    KC_A,       KC_R,       KC_S,       KC_T,       KC_G,       KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_QUOT,                            KC_ENT,     KC_PGUP,
     KC_LSFT,                KC_X,       KC_C,       KC_D,       KC_V,       KC_Z,       KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSFT,    KC_UP,      KC_PGDN,
-    KC_LCTL,    KC_LALT,    KC_LGUI,                                        KC_SPC,                             KC_RGUI,    MO(1),      KC_RCTL,                KC_LEFT,    KC_DOWN,    KC_RGHT),
+    KC_LGUI,    KC_LCTL,    KC_LALT,                                        KC_SPC,                             KC_RGUI,    MO(1),      KC_RCTL,                KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 // layer Mac Fn
 [1] = LAYOUT_75_ansi(
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_P,       KC_B,       KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_LBRC,    KC_RBRC,                KC_BSLS,    KC_END,
     KC_CAPS,    KC_A,       KC_R,       KC_S,       KC_T,       KC_G,       KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_QUOT,                            KC_ENT,     KC_PGUP,
     KC_LSFT,                KC_X,       KC_C,       KC_D,       KC_V,       KC_Z,       KC_K,       KC_H,       KC_COMM,    KC_DOT,     KC_SLSH,                KC_RSFT,    KC_UP,      KC_PGDN,
-    KC_LCTL,    KC_LGUI,    KC_LALT,                                        KC_SPC,                             KC_RALT,    MO(3),      KC_RCTL,                KC_LEFT,    KC_DOWN,    KC_RGHT),
+    KC_LGUI,    KC_LCTL,    KC_LALT,                                        KC_SPC,                             KC_RALT,    MO(3),      KC_RCTL,                KC_LEFT,    KC_DOWN,    KC_RGHT),
 
 // layer win Fn
 [3] = LAYOUT_75_ansi(
